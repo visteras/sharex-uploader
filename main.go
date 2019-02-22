1bb01e287c9e4026a39493030f23a0db02118ba0
Dmitriy Dergachev <dergachevdv90@gmail.com>
2019-02-22 15:25:53 +0300

    Add RegexpHandler

--- main.go
+++ main.go
@@ -10,18 +10,56 @@ import (
 	"mime/multipart"
 	"net/http"
 	"os"
+	"path/filepath"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
 )
 
+type route struct {
+	pattern	*regexp.Regexp
+	handler	http.Handler
+}
+
+type RegexpHandler struct {
+	routes	[]*route
+}
+
+func (h *RegexpHandler) Handler(pattern *regexp.Regexp, handler http.Handler) {
+	h.routes = append(h.routes, &route{pattern, handler})
+}
+
+func (h *RegexpHandler) HandleFunc(pattern *regexp.Regexp, handler func(http.ResponseWriter, *http.Request)) {
+	h.routes = append(h.routes, &route{pattern, http.HandlerFunc(handler)})
+}
+
+func (h *RegexpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	for _, route := range h.routes {
+		if route.pattern.MatchString(r.URL.Path) {
+			log.Printf("Request URI %s\n", r.URL.Path)
+			route.handler.ServeHTTP(w, r)
+			return
+		}
+	}
+
+	http.NotFound(w, r)
+}
+
+var VirtualHost string
+
 func init() {
 	rand.Seed(time.Now().UnixNano())
-
 }
+
 func main() {
-	http.HandleFunc("/", upload)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	VirtualHost = os.Getenv("VIRTUAL_HOST")
+	handler := &RegexpHandler{}
+	handler.HandleFunc(regexp.MustCompile(`\/upload`), UploadFile)
+	handler.HandleFunc(regexp.MustCompile(`\/[a-zA-Z0-9]{16}\.(.*)`), ShowFile)
+
+	//http.HandleFunc("/", upload)
+	log.Fatal(http.ListenAndServe(":3000", handler))
 }
 
 var letterRunes = []rune("1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
@@ -34,91 +72,131 @@ func RandStringRunes(n int) string {
 	return string(b)
 }
 
-func upload(w http.ResponseWriter, r *http.Request) {
+//func upload(w http.ResponseWriter, r *http.Request) {
+//
+//	if r.Method == http.MethodPost {
+//		UploadFile(w, r)
+//	}
+//
+//	if r.Method == http.MethodGet {
+//		parts := strings.Split(r.URL.Path, "/")
+//		path := "./files/" + parts[1]
+//		e := false
+//		if _, err := os.Stat(path); !os.IsNotExist(err) {
+//			file, err := os.Open(path)
+//			if err != nil {
+//				e = true
+//				fmt.Fprintf(w, "1. %v", err)
+//			}
+//
+//			buff := make([]byte, 512)
+//			file.Seek(0, 0)
+//			_, err = file.Read(buff)
+//			file.Seek(0, 0)
+//
+//			if err != nil && err != io.EOF {
+//				e = true
+//				fmt.Fprintf(w, "5. %v", err)
+//			} else {
+//				contentType := http.DetectContentType(buff)
+//				writeImage(w, file, contentType)
+//			}
+//		} else {
+//			e = true
+//		}
+//		if e == true {
+//			w.WriteHeader(404)
+//		}
+//	}
+//
+//}
 
+//func writeImage(w http.ResponseWriter, file *os.File, mime string) {
+//
+//	w.Header().Set("Content-Type", mime)
+//	fi, err := file.Stat()
+//	if err != nil {
+//		log.Println("cant read file size")
+//		w.WriteHeader(http.StatusInternalServerError)
+//	} else {
+//		buff := make([]byte, fi.Size())
+//		file.Read(buff)
+//		w.Header().Set("Content-Length", strconv.Itoa(len(buff)))
+//		if _, err := w.Write(buff); err != nil {
+//			log.Println("unable to write image.")
+//		}
+//	}
+//}
+
+func UploadFile(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodPost {
-
-		UploadFile(w, r)
-
+		file, handle, err := r.FormFile("data")
+		if err != nil {
+			log.Println("Can't get file in the request")
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		defer file.Close()
+		mimeType := handle.Header.Get("Content-Type")
+		switch mimeType {
+		case "image/jpeg", "image/png", "image/gif":
+			saveFile(w, file, handle)
+		default:
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+		}
+	} else {
+		http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
 	}
+}
 
+func ShowFile(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
-		parts := strings.Split(r.URL.Path, "/")
-		path := "./files/" + parts[1]
+		filename := strings.Split(r.URL.Path, "/")[1]
+		path := filepath.Join("./files", filename)
 		e := false
 		if _, err := os.Stat(path); !os.IsNotExist(err) {
-			file, err := os.Open(path)
-			if err != nil {
+			if file, err := os.Open(path); err != nil {
 				e = true
-				fmt.Fprintf(w, "1. %v", err)
-			}
-
-			buff := make([]byte, 512)
-			file.Seek(0, 0)
-			_, err = file.Read(buff)
-			file.Seek(0, 0)
-
-			if err != nil && err != io.EOF {
-				e = true
-				fmt.Fprintf(w, "5. %v", err)
 			} else {
-				contentType := http.DetectContentType(buff)
-				writeImage(w, file, contentType)
+				buff := make([]byte, 512)
+				file.Seek(0, 0)
+				_, err = file.Read(buff)
+				file.Seek(0, 0)
+				if err != nil && err != io.EOF {
+					e = true
+				} else {
+					w.Header().Set("Content-Type", http.DetectContentType(buff))
+					fi, err := file.Stat()
+					if err != nil {
+						log.Printf("Can't read file size: %v\n", fi.Name())
+						w.WriteHeader(http.StatusInternalServerError)
+					} else {
+						buff := make([]byte, fi.Size())
+						file.Read(buff)
+						w.Header().Set("Content-Length", strconv.Itoa(len(buff)))
+						if _, err := w.Write(buff); err != nil {
+							log.Printf("Unable to write file in response: %v\n", fi.Name())
+							w.WriteHeader(http.StatusInternalServerError)
+						}
+					}
+				}
 			}
 		} else {
 			e = true
 		}
 		if e == true {
-			w.WriteHeader(404)
+			http.NotFound(w, r)
 		}
-	}
-
-}
-
-func writeImage(w http.ResponseWriter, file *os.File, mime string) {
-
-	w.Header().Set("Content-Type", mime)
-	fi, err := file.Stat()
-	if err != nil {
-		log.Println("cant read file size")
-		w.WriteHeader(http.StatusInternalServerError)
 	} else {
-		buff := make([]byte, fi.Size())
-		file.Read(buff)
-		w.Header().Set("Content-Length", strconv.Itoa(len(buff)))
-		if _, err := w.Write(buff); err != nil {
-			log.Println("unable to write image.")
-		}
+		http.Error(w, http.StatusText(http.StatusForbidden), http.StatusForbidden)
 	}
-}
-
-func UploadFile(w http.ResponseWriter, r *http.Request) {
-	file, handle, err := r.FormFile("data")
-	if err != nil {
-		fmt.Fprintf(w, "0. %v", err)
-		return
-	}
-	defer file.Close()
-
-	mimeType := handle.Header.Get("Content-Type")
-	switch mimeType {
-	case "image/jpeg":
-		saveFile(w, file, handle)
-	case "image/png":
-		saveFile(w, file, handle)
-	case "image/gif":
-		saveFile(w, file, handle)
-	default:
-		w.WriteHeader(http.StatusBadRequest)
-	}
-
-	//saveFile(w, file, handle)
 }
 
 func saveFile(w http.ResponseWriter, file multipart.File, handle *multipart.FileHeader) {
 	data, err := ioutil.ReadAll(file)
 	if err != nil {
-		fmt.Fprintf(w, "3. %v", err)
+		log.Printf("Can't read temporary file: %v\n", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 
@@ -134,14 +212,15 @@ func saveFile(w http.ResponseWriter, file multipart.File, handle *multipart.File
 		}
 	}
 
-	err = ioutil.WriteFile("./files/"+fileName, data, 0666)
+	err = ioutil.WriteFile("./files/" + fileName, data, 0666)
 	if err != nil {
-		fmt.Fprintf(w, "4. %v", err)
+		log.Printf("Can't write file \"%s\": %v", fileName, err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
 	jsonResponse(w, http.StatusCreated, Response{
-		URI: "http://" + os.Getenv("VIRTUAL_HOST") + "/" + fileName,
-		//DelURI: "http://" + os.Getenv("VIRTUAL_HOST") + "/delete" + handle.Filename,
+		URI: fmt.Sprintf("http://%s/%s", VirtualHost, fileName),
+		//DelURI: "http://" + os.Getenv("VirtualHost") + "/delete" + handle.Filename,
 	})
 }
 
@@ -150,7 +229,8 @@ func jsonResponse(w http.ResponseWriter, code int, message Response) {
 	w.WriteHeader(code)
 	err := json.NewEncoder(w).Encode(&message)
 	if err != nil {
-		fmt.Fprintf(w, "%v", err)
+		log.Println(err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 	}
 }
 
